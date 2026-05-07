6a604f1c19b52203d7d903ae05a0805864daccdc
John Patrick Valera <johnpatrickyusoresvalera@gmail.com>
2026-05-07 09:33:17 +0000

    clear all project contents except reports2

--- filings/playwright/test.ts
+++
@@ -1,9 +0,0 @@
-import { test as base, expect } from "@playwright/test";
-import { setCurrentPage } from "./pageContext";
-
-export const test = base;
-export { expect };
-
-base.beforeEach(async ({ page }) => {
-  setCurrentPage(page);
-});
